ffb296b08aa8ea129461c504da9aa9d9b11f917a
Dongkeun.YOON <31213371+Bronzeroot@users.noreply.github.com>
2022-08-15 00:19:29 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,7 +6,9 @@ import { useState } from 'react';
 function App() {
 
     let post = 'happy birthday';
-    let [title, b] = useState('Raincoat');
+    let [subject, a] = useState('GA4');
+    let [title, b] = useState(['Raincoat','Boots','Umbrella']);
+    let [like, likechange] = useState(0);
     let [logo, setlogo] = useState('ReactProject');
     //logo같이 잘 안변하는 친구는 state쓸 필요가 없음
     
@@ -15,23 +18,42 @@ function App() {
     // > let c = num[1];
 
     //state를 쓰는 html은 변수가 바뀌면 자동으로 렌더링이 다시 된다
-
+  function 함수(){
+    console.log(1);
+  }
+  // ()=> {console.log(1)}
+  // 위 코드는 함수를 만드는 기능
+  // State는 등호로 변경이 안됨
   return (
     <div className="App">
         <div className="black-nav">
         <h4 style={ {color : 'green',fontSize : '16px'} }>myblog</h4>
       </div>
       <div className="list">
-        <h4>{ title }</h4>
+        <h4>{ title[0] } <span onClick={ ()=>{likechange(like+1) }}>like</span> 
+        { like } </h4>
+        <span onClick={()=>{
+
+          let copy = [...title];
+          // ...을 통해서 array풀었다가 다시 넣어서.. 새로운 ref data
+          copy[0]='Jacket';
+          b(copy);
+        }}>change</span>
         <p>220731 created</p>
       </div>
+      
       <div className="list">
-        <h4>{ post }</h4>
+        <h4>{ title[1] }</h4>
+        <p>220731 created</p>
+      </div>
+
+      <div className="list">
+        <h4>{ title[2] }</h4>
         <p>220731 created</p>
       </div>
       <div className="list">
         <h4>{ logo }</h4>
-        <p>220731 created</p>
+        <p>220814 created</p>
       </div>
       <h4>{ post }</h4>
     </div>
